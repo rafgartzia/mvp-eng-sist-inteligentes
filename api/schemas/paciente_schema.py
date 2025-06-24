9fbfc2d0fa4bcba41d5aabef43fb470034b663e5
garcia <rafgarcia@gmail.com>
2025-06-24 10:28:15 -0300

    modificacoes no schema

--- api/schemas/paciente_schema.py
+++ api/schemas/paciente_schema.py
@@ -1,55 +1,55 @@
 from pydantic import BaseModel
 from typing import Optional, List
 from model.paciente import Paciente
-import json
-import numpy as np
+
 
 class PacienteSchema(BaseModel):
     """ Define como um novo paciente a ser inserido deve ser representado
     """
-    name: str = "Maria"
-    preg: int = 6
-    plas: int = 148
-    pres: int = 72
-    skin: int = 35
-    test: int = 2
-    mass: float = 33.6
-    pedi: float = 0.627
-    age: int = 50
-    
+    name: str = "Tatiana"
+    age: int = 30
+    systolicBP: int = 100
+    diastolicBP: int = 70
+    bs: int = 90
+    bodyTemp: float = 36.5
+    heartRate: int = 80
+
+
 class PacienteViewSchema(BaseModel):
     """Define como um paciente será retornado
     """
     id: int = 1
-    name: str = "Maria"
-    preg: int = 6
-    plas: int = 148
-    pres: int = 72
-    skin: int = 35
-    test: int = 0
-    mass: float = 33.6
-    pedi: float = 0.627
-    age: int = 50
-    outcome: int = None
-    
+    name: str = "Tatiana"
+    age: int = 30
+    systolicBP: int = 100
+    diastolicBP: int = 70
+    bs: int = 90
+    bodyTemp: float = 36.5
+    heartRate: int = 80
+    riskLevel: float = None
+
+
 class PacienteBuscaSchema(BaseModel):
     """Define como deve ser a estrutura que representa a busca.
     Ela será feita com base no nome do paciente.
     """
-    name: str = "Maria"
+    name: str = "Tatiana"
+
 
 class ListaPacientesSchema(BaseModel):
     """Define como uma lista de pacientes será representada
     """
     pacientes: List[PacienteSchema]
 
-    
+
 class PacienteDelSchema(BaseModel):
     """Define como um paciente para deleção será representado
     """
-    name: str = "Maria"
-    
-# Apresenta apenas os dados de um paciente    
+    name: str = "Tatiana"
+
+# Apresenta apenas os dados de um paciente
+
+
 def apresenta_paciente(paciente: Paciente):
     """ Retorna uma representação do paciente seguindo o schema definido em
         PacienteViewSchema.
@@ -57,18 +57,18 @@ def apresenta_paciente(paciente: Paciente):
     return {
         "id": paciente.id,
         "name": paciente.name,
-        "preg": paciente.preg,
-        "plas": paciente.plas,
-        "pres": paciente.pres,
-        "skin": paciente.skin,
-        "test": paciente.test,
-        "mass": paciente.mass,
-        "pedi": paciente.pedi,
         "age": paciente.age,
-        "outcome": paciente.outcome
+        "systolicBP": paciente.systolicBP,
+        "diastolicBP": paciente.diastolicBP,
+        "bs": paciente.bs,
+        "bodyTemp": paciente.bodyTemp,
+        "heartRate": paciente.heartRate,
+        "riskLevel": paciente.riskLevel
     }
-    
+
 # Apresenta uma lista de pacientes
+
+
 def apresenta_pacientes(pacientes: List[Paciente]):
     """ Retorna uma representação do paciente seguindo o schema definido em
         PacienteViewSchema.
@@ -78,16 +78,13 @@ def apresenta_pacientes(pacientes: List[Paciente]):
         result.append({
             "id": paciente.id,
             "name": paciente.name,
-            "preg": paciente.preg,
-            "plas": paciente.plas,
-            "pres": paciente.pres,    
-            "skin": paciente.skin,
-            "test": paciente.test,
-            "mass": paciente.mass,
-            "pedi": paciente.pedi,
             "age": paciente.age,
-            "outcome": paciente.outcome
+            "systolicBP": paciente.systolicBP,
+            "diastolicBP": paciente.diastolicBP,
+            "bs": paciente.bs,
+            "bodyTemp": paciente.bodyTemp,
+            "heartRate": paciente.heartRate,
+            "riskLevel": paciente.riskLevel
         })
 
     return {"pacientes": result}
-
